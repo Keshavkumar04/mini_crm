e21628721c78a90a2e496c104e88b7b59dca442f
Keshavkumar04 <97456331+Keshavkumar04@users.noreply.github.com>
2024-11-19 00:09:30 +0530

    Your commit message here

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,26 @@
-// import Link from "next/link";
+import Link from "next/link";
 
-// export default function HomePage() {
-//   return (
-//     <div style={{ padding: "20px" }}>
-//       <h1>Mini CRM & Campaign Management</h1>
-//       <ul>
-//         <li>
-//           <Link href="/customers">Add Customers</Link>
-//         </li>
-//         <li>
-//           <Link href="/orders">Add Orders</Link>
-//         </li>
-//         <li>
-//           <Link href="/audience">Create Audience</Link>
-//         </li>
-//         <li>
-//           <Link href="/campaigns">View Campaign History</Link>
-//         </li>
-//         <li>
-//           <Link href="/sendMessage">Send Messages</Link>
-//         </li>
-//       </ul>
-//     </div>
-//   );
-// }
+export default function HomePage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Mini CRM & Campaign Management</h1>
+      <ul>
+        <li>
+          <Link href="/customers">Add Customers</Link>
+        </li>
+        <li>
+          <Link href="/orders">Add Orders</Link>
+        </li>
+        <li>
+          <Link href="/audience">Create Audience</Link>
+        </li>
+        <li>
+          <Link href="/campaigns">View Campaign History</Link>
+        </li>
+        <li>
+          <Link href="/sendMessage">Send Messages</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
